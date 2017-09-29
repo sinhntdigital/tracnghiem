3a39004c839029eaa3bc631f2085f78ef94a7209
sinhntdigital <upworksinh@gmail.com>
2017-09-29 08:18:41 +0700

    change default column start_dong in table user

--- database/migrations/2017_09_27_063235_add_column_start_doing_to_users_table.php
+++ database/migrations/2017_09_27_063235_add_column_start_doing_to_users_table.php
@@ -14,7 +14,7 @@ class AddColumnStartDoingToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function($table){
-            $table->dateTime("start_doing");
+            $table->dateTime("start_doing")->nullable();
         });
     }
 
