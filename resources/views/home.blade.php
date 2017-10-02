3db973d6868d133cd1ed59b19cd22e65b3adad2b
sinhntdigital <upworksinh@gmail.com>
2017-10-02 17:30:49 +0700

    try write quiz

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,10 +10,34 @@
                     <b> noi dung question</b>
                 </div>
                 <div class="select_queston">
-                    chon cau tra loi
+                    <?php 
+                        $questions=\App\User::join('role_user','users.id','=','role_user.user_id')->join('roles','roles.id','=','role_user.role_id')->join('exams','exams.user_id','=','users.id')->join('questions','exams.id','=','questions.exam_id')->join('content_answers','content_answers.question_id','=','questions.id')->where('role_user.user_id',Auth::user()->id)->groupBy('questions.id')->paginate(1);
+
+                        $answers=\App\User::join('role_user','users.id','=','role_user.user_id')->join('roles','roles.id','=','role_user.role_id')->join('exams','exams.user_id','=','users.id')->join('questions','exams.id','=','questions.exam_id')->join('content_answers','content_answers.question_id','=','questions.id')->where('role_user.user_id',Auth::user()->id)->get();
+                        
+                      dump($questions);
+                    ?>
+                
+
+                        @foreach ($questions as $question)
+            
+                       <B>{{ $question->title_question }}</B><br>
+                            {{ $question->content_question }}<br>
+                            @foreach ($answers as $answer)
+                               @if($question->question_id == $answer->question_id) 
+                                    {{$answer->content_answer}}
+                                @endif
+                            @endforeach
+                               
+
+                        @endforeach
+
+                  
+                    
                 </div>
             </div>
         </div>
     </div>
 </div>
+{!! $questions->render() !!}
 @endsection
