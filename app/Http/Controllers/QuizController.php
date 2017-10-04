da9599529c57d080fd78ab90970b376b6162731b
sinhntdigital <upworksinh@gmail.com>
2017-10-04 11:07:43 +0700

    finish process when add table user_answer

--- app/Http/Controllers/QuizController.php
+++ app/Http/Controllers/QuizController.php
@@ -19,23 +19,33 @@ class QuizController extends Controller
 
     public function index($examId){
     	$questions=\App\User::join('role_user','users.id','=','role_user.user_id')->join('roles','roles.id','=','role_user.role_id')->join('exams','users.id','=','exams.user_id')->leftjoin('questions','exams.id','=','questions.exam_id')->join('content_answers','questions.id','=','content_answers.question_id')->where('role_user.user_id',Auth::user()->id)->where('exam_id','=',$examId)->groupBy('question_id')->paginate(1);
-        $answers=\App\User::join('role_user','users.id','=','role_user.user_id')->join('roles','roles.id','=','role_user.role_id')->join('exams','users.id','=','exams.user_id')->leftjoin('questions','exams.id','=','questions.exam_id')->join('content_answers','questions.id','=','content_answers.question_id')->where('role_user.user_id',Auth::user()->id)->where('exam_id','=',$examId)->select('*','content_answers.id as content_answer_id')->get();
+        $answers=\App\User::join('role_user','users.id','=','role_user.user_id')->join('roles','roles.id','=','role_user.role_id')->join('exams','users.id','=','exams.user_id')->leftjoin('questions','exams.id','=','questions.exam_id')->join('content_answers','questions.id','=','content_answers.question_id')->leftjoin('user_answers','content_answers.id','=','user_answers.content_answer_id')->where('role_user.user_id',Auth::user()->id)->where('exam_id','=',$examId)->select('*','content_answers.id as content_answer_id')->get();
+        $userA=\App\UserAnswer::where('user_answers.content_answer_id', $examId)->where('user_answers.user_id',Auth::user()->id)->first();
         $exam=\App\Exam::where('id', $examId)->first();
 
         if($exam->start_doing==null or (strtotime($exam->end_doing) - strtotime('now') ) <= 0 )
         	return redirect('listquiz');
         else
-    		return view('member.quiz',compact('examId','answers','questions','exam'));
+    		return view('member.quiz',compact('examId','answers','questions','exam','userA'));
     }
     /*________update answer________*/
     public function updateAnswer() {
-    	$getUAns=\App\ContentAnswer::where('id', $_GET['contentA_id'])->first();
-    	if($getUAns->user_answer==0)
-	    	\App\ContentAnswer::where('id', $_GET['contentA_id'])
-	          ->update(['user_answer' => 1]);
-	    else
-	    	\App\ContentAnswer::where('id', $_GET['contentA_id'])
-	          ->update(['user_answer' => 0]);
+    	$userA=\App\UserAnswer::where('user_answers.content_answer_id', $_GET['contentA_id'])->where('user_answers.user_id',Auth::user()->id)->first();
+    	$iUserA = new \App\UserAnswer;
+    	if ($userA==null)
+    	{
+        	$iUserA->user_id = Auth::user()->id;
+        	$iUserA->content_answer_id = $_GET['contentA_id'];
+        	$iUserA->user_answer = 1;
+        	$iUserA->save();
+    	}
+    	else {
+				if($userA->user_answer==0)
+					$userA=\App\UserAnswer::where('user_answers.content_answer_id', $_GET['contentA_id'])->where('user_answers.user_id',Auth::user()->id)->update(['user_answer' => 1]);
+				else
+					$userA=\App\UserAnswer::where('user_answers.content_answer_id', $_GET['contentA_id'])->where('user_answers.user_id',Auth::user()->id)->update(['user_answer' => 0]);
+	    	}
+    	
     }
      /*________list quiz________*/
     public function listQuiz() {
---
+++ database/migrations/2017_10_04_090753_create_user_answer_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserAnswerTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_answers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->integer('content_answer_id')->unsigned();
+            $table->foreign('content_answer_id')->references('id')->on('content_answers');
+            $table->boolean('user_answer');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_answers');
+    }
+}
